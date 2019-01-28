1f85b72a56b3baea3026ec9c07918d244e1fc1e7
Jhonata Sobrinho <jhonata.sobrinho@mercadolivre.com>
2019-01-28 17:17:17 -0200

    Adding config of timeout on r4j

--- src/main/java/com/newgen/cloudstrategies/CloudStrategiesApplication.java
+++ src/main/java/com/newgen/cloudstrategies/CloudStrategiesApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableFeignClients
+@SpringBootApplication
 
 public class CloudStrategiesApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(CloudStrategiesApplication.class, args);
     }
